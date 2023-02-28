def7e64d01791af30ed3ec82e3bc2fcafb0cc1db
zinzoddari <zinzoddari@gmail.com>
2023-02-28 23:36:32 +0900

    docs: 기본 환경설정 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+- java 11
+- spring boot 2.7.2
+- jpa
---
+++ src/main/java/com/ezic/config/jpa/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.ezic.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.ezic")
+public class JpaConfig {
+}
