b17ba560516d1a5c4f1f15e8992ee96b880bf987
zinzoddari <zinzoddari@gmail.com>
2023-02-28 23:13:06 +0900

    init: first commit

---
+++ src/main/java/com/ezic/EzicServerApplication.java
@@ -0,0 +1,13 @@
+package com.ezic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzicServerApplication.class, args);
+	}
+
+}
