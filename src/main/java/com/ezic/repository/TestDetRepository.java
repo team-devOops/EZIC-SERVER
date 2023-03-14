35c77f103607b0a1737ee5d0781586a63aeeb037
zinzoddari <zinzoddari@gmail.com>
2023-03-14 20:19:19 +0900

    refactor: 사용 안하는 내용 삭제

--- src/main/java/com/ezic/repository/TestDetRepository.java
+++ src/main/java/com/ezic/repository/TestDetRepository.java
@@ -1,7 +1,6 @@
 package com.ezic.repository;
 
 import com.ezic.domain.TestDet;
-import com.ezic.domain.TestMst;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
